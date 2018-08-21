a71d210c9c5190fd97a5a5211b89f303b592aec9
JimBoonie <mcgough.mason@gmail.com>
2018-08-21 17:50:10 -0400

    Create script to make animation of wireframe;

---
+++ animation.py
@@ -0,0 +1,79 @@
+"""
+=====
+Decay
+=====
+
+This example showcases a sinusoidal decay animation.
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
+
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+nrows, ncols = X.shape[0], Y.shape[0]
+rii = list(xrange(0, nrows))
+cii = list(xrange(0, ncols))
+
+Xm, Ym = np.meshgrid(X, Y)
+Zm = np.zeros(Xm.shape[0])
+X, Y, Z = np.broadcast_arrays(Xm, Ym, Zm)
+
+f = lambda x, y, t : (x + t * np.sin(y), y + t * np.sin(x))
+
+def data_gen(t=0):
+    cnt = 0
+    while cnt < 1000:
+        cnt += 1
+        t += 0.001
+        x, y = f(X, Y, t)
+        tx, ty, tZ = np.transpose(x), np.transpose(y), np.transpose(Z)
+
+        xlines = [x[i] for i in rii]
+        ylines = [y[i] for i in rii]
+        zlines = [Z[i] for i in rii]
+
+        txlines = [tx[i] for i in cii]
+        tylines = [ty[i] for i in cii]
+        tzlines = [tZ[i] for i in cii]
+
+        lines = ([list(zip(xl, yl, zl))
+                    for xl, yl, zl in zip(xlines, ylines, zlines)]
+                + [list(zip(xl, yl, zl))
+                    for xl, yl, zl in zip(txlines, tylines, tzlines)])
+
+
+        yield lines
+
+
+def init():
+    ax.set_xlim(-4.0, 4.0)
+    ax.set_ylim(-4.0, 4.0)
+    ax.set_zlim(-1.0, 1.0)
+    return surf,
+
+# Create figure.
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_wireframe([], [], [], 
+                         linewidth=1, antialiased=False,
+                         rstride=25, cstride=25)
+xdata, ydata, zdata = [], [], []
+# plt.title('f(x, y)')
+
+
+def run(data):
+    # update the data
+    lines = data
+    surf.set_segments(lines)
+
+    return surf,
+
+ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10,
+                              repeat=False, init_func=init)
+plt.show()
